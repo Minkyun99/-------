959304e919f1236dbf733c4da3374b83b024f4e0
minkyun99 <alsrud7412@naver.com>
2024-08-22 15:57:19 +0900

    20240822

--- 2회차/javascript/script.js
+++ 2회차/javascript/script.js
@@ -5,4 +5,22 @@ jQuery(document).ready(function(){
     }).mouseout(function(){
         $(this).find('.submenulist').stop().slideUp(500)
     })
+
+    
+    $('.imgslide div li').last().prependTo('.imgslide div');
+    $('.imgslide div').css('left',-800);
+    setInterval(function(){
+       $('.imgslide div').animate({left:'-='+800},'slow',function(){
+           $('.imgslide div li') .first().appendTo('.imgslide div');
+           $('.imgslide div').css('left',-800);
+       });
+    },2000);
+
+    $('.notice li').click(function(){
+        $('.modal').addClass('active')        
+    })
+
+    $('.modal_close').click(function(){
+        $('.modal').removeClass('active')
+    })
 })
